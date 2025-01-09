87109c62cee2d1fc8bd5dbd6ccfcb7f4f1fbad3d
Marta <veeenling@gmail.com>
2025-01-09 14:00:52 +0200

    second run

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
 
 const router = createBrowserRouter([
-    {path:'/', element: <div>MainLayout1</div>},
-    {path: '/', element: <div>MainLayout2</div>},
-    {path: '/', element: <div>MainLayout3</div>}
+    {path:'/', element: <MainLayout/>}
 ])
 
 createRoot(document.getElementById('root')!)
