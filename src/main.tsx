e31e8302bbdac43b41143491f81ffb2db861aa4b
Marta <veeenling@gmail.com>
2025-01-09 13:49:26 +0200

    first run

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(<App/>)
+const router = createBrowserRouter([
+    {path:'/', element: <div>MainLayout1</div>},
+    {path: '/', element: <div>MainLayout2</div>},
+    {path: '/', element: <div>MainLayout3</div>}
+])
+
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router = {router}/>)
