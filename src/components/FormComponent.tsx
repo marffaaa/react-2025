981d78b373d7bc6588e5178506b54b2bc3a52e85
Marta <veeenling@gmail.com>
2025-01-15 18:02:24 +0200

    the second way

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -11,27 +11,38 @@ const FormComponent = () => {
     })
     const handlerSubmit = (e:FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        let user ={
+            username: formState.username,
+            password: formState.password
+        }
+        console.log(user)
         // const form = e.target as HTMLFormElement
         // console.log(form.username)
         // console.log(form.username.value)
         // console.log(form.password)
         // console.log(form.password.value)
     };
-    const handleUsernameChange = (e:FormEvent<HTMLInputElement>) => {
+    // const handleUsernameChange = (e:FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLInputElement;
+    //     console.log(input.value);
+    //     setFormState({...formState, username: input.value})
+    // };
+    // const handlePasswordChange = (e:FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLInputElement;
+    //     console.log(input.value);
+    //     setFormState({...formState, password: input.value})
+    // };
+    const handleInputChange = (e:FormEvent<HTMLInputElement>) => {
         const input = e.target as HTMLInputElement;
         console.log(input.value);
-        setFormState({...formState, username: input.value})
-    };
-    const handlePasswordChange = (e:FormEvent<HTMLInputElement>) => {
-        const input = e.target as HTMLInputElement;
-        console.log(input.value);
-        setFormState({...formState, password: input.value})
+        setFormState({...formState, [input.name]: input.value})
     };
     return (
         <div>
             <form onSubmit={handlerSubmit}>
-                <input type="text" name='username' value={formState.username} onChange={handleUsernameChange}/>
-                <input type="text" name='password' value={formState.password} onChange={handlePasswordChange}/>
+                <input type="text" name='username' value={formState.username} onChange={handleInputChange}/>
+                <input type="text" name='password' value={formState.password} onChange={handleInputChange}/>
                 <button>send</button>
             </form>
         </div>
