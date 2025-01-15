84faa6642909dd7589dd8c80f3391ec66db5052a
Marta <veeenling@gmail.com>
2025-01-15 15:43:57 +0200

    event control

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,11 +1,15 @@
-
+import { FormEvent } from "react";
 
 const FormComponent = () => {
+    const handler = (e: FormEvent<HTMLInputElement>) => {
+        console.log(e)
+    };
     return (
         <div>
             <form>
-                <input type="text" name='username'/>
+                <input type="text" name='username' onChange={handler}/>
                 <input type="text" name='password'/>
+                <button>send</button>
             </form>
         </div>
     );
