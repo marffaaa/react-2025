ccd3f51c7387ce9211bbbc1f8887f42622c1366d
Marta <veeenling@gmail.com>
2025-02-01 20:17:07 +0200

    add some redux func

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,59 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from 'react-router-dom'
 import {routes} from './routes/routes.tsx'
+import { Provider, useSelector } from 'react-redux'
+import {PayloadAction, configureStore, createSlice } from '@reduxjs/toolkit'
+import { IUser } from './models/IUser.ts'
+import { IRecipe } from './models/IRecipe.ts'
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>)
+type UserSliceType={
+    users : IUser[]
+}
+
+const userInitialState: UserSliceType= {users:[]}
+
+type RecipeSliceType={
+    recipes : IRecipe[]
+}
+
+const recipeInitialState: RecipeSliceType= {recipes:[]}
+
+const userSlice = createSlice({
+    name: "userSlice",
+    initialState: userInitialState,
+    reducers:{
+        LoadUsers: (state, action:PayloadAction<IUser[]>)=>{
+           state.users = action.payload
+        }
+    }
+})
+
+export const userSliceAction = {
+    ...userSlice.actions
+}
+
+const recipeSlice = createSlice({
+    name: "recipeSlice",
+    initialState: recipeInitialState,
+    reducers:{
+        LoadUsers: (state, action:PayloadAction<IRecipe[]>)=>{
+            state.recipes = action.payload
+        }
+    }
+})
+
+export const useAppSelector = useSelector.withTypes<ReturnType<typeof store.getState>>();
+
+const store = configureStore({
+    reducer:{
+        userSlice: userSlice.reducer,
+        recipeSlice: recipeSlice.reducer
+    }
+});
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <RouterProvider router={routes}/>
+    </Provider>
+
+)
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,7 +1,9 @@
 import UsersComponent from "../components/users/UsersComponent";
 import SearchComponent from "../components/search/SearchComponent";
+// import { useAppSelector } from "../main";
 
 const UsersPage = () => {
+    // const userSlice = useAppSelector(state => state.userSlice)
     return (
         <div>
             <SearchComponent />
